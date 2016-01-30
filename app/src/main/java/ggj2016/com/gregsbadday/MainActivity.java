3f3a69d0351d6fdf6dcdedbfb0c2bf190b51764a
Sami GauthierDickey <sgfirepaw@gmail.com>
2016-01-29 23:04:35 -0700

    hello world activity

---
+++ app/src/main/java/ggj2016/com/gregsbadday/MainActivity.java
@@ -0,0 +1,16 @@
+package ggj2016.com.gregsbadday;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sami on 1/29/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
