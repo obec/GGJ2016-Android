842f0ffbb8d7a8cc03a4c0fd527a9f7dc53b499b
Brandon Davis <brandon.davis@possible.com>
2016-01-31 13:45:25 -0700

    add toasts

--- android/src/com/ggj2016/gregsbadday/MainActivity.java
+++ android/src/com/ggj2016/gregsbadday/MainActivity.java
@@ -354,8 +354,10 @@ public class MainActivity extends AppCompatActivity {
 
         if (resultCode == RESULT_TIME_UP) {
             roundOver();
+            Toast.makeText(this, "TIMES UP!", Toast.LENGTH_LONG);
         } else {
             mHandler.postDelayed(mRoundOverRunnable, getTimeRemaining());
+            Toast.makeText(this, "GOOD JOB!", Toast.LENGTH_LONG);
         }
     }
 
