805ed6d1d001a5f93e8d87fe4f15b759d1ff231d
Brandon Davis <brandon.davis@possible.com>
2016-01-30 22:16:10 -0700

    use colored images to check collision

--- android/src/com/ggj2016/gregsbadday/TraceView.java
+++ android/src/com/ggj2016/gregsbadday/TraceView.java
@@ -9,23 +9,23 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.PorterDuff;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
-import android.widget.Toast;
 
 import com.plattysoft.leonids.ParticleSystem;
 
-public class TraceView extends View {
+import timber.log.Timber;
 
-    private static final String TAG = TraceView.class.getSimpleName();
+public class TraceView extends View {
 
     private static final float STROKE_WIDTH = 5.0f;
     private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
@@ -39,41 +39,10 @@ public class TraceView extends View {
     private ParticleSystem particleSystem;
     private Activity activity;
 
-    private static final int rectWidth = 50;
-
-    private static final int pointOneLeft = 98;
-    private static final int pointOneRight = pointOneLeft + rectWidth;
-    private static final int pointOneTop = 210;
-    private static final int pointOneBottom = pointOneTop + rectWidth;
-
-    private static final int pointTwoLeft = 270;
-    private static final int pointTwoRight = pointTwoLeft + rectWidth;
-    private static final int pointTwoTop = 210;
-    private static final int pointTwoBottom = pointTwoTop + rectWidth;
-
-    private static final int pointThreeLeft = 421;
-    private static final int pointThreeRight = pointThreeLeft + rectWidth;
-    private static final int pointThreeTop = 210;
-    private static final int pointThreeBottom = pointThreeTop + rectWidth;
-
-    private static final int pointFourLeft = 270;
-    private static final int pointFourRight = pointFourLeft + rectWidth;
-    private static final int pointFourTop = 410;
-    private static final int pointFourBottom = pointFourTop + rectWidth;
-
-    private static final int pointFiveLeft = 270;
-    private static final int pointFiveRight = pointFiveLeft + rectWidth;
-    private static final int pointFiveTop = 620;
-    private static final int pointFiveBottom = pointFiveTop + rectWidth;
-
-    private RectF pointOneRect;
-    private RectF pointTwoRect;
-    private RectF pointThreeRect;
-    private RectF pointFourRect;
-    private RectF pointFiveRect;
-
     Bitmap backgroundBitmap;
     Drawable tintedBackgroundDrawable;
+
+    private Point windowSize = new Point();
 
     public TraceView(Context context) {
         this(context, null);
@@ -116,21 +85,13 @@ public class TraceView extends View {
             setBackground(tintedBackgroundDrawable);
         }
 
-        setupRects();
-
         if (context instanceof Activity) {
             activity = (Activity) context;
+            Display display = activity.getWindowManager().getDefaultDisplay();
+            display.getSize(windowSize);
         } else {
             throw new IllegalStateException("try again with an activity context");
         }
-    }
-
-    private void setupRects() {
-        pointOneRect = new RectF(pointOneLeft, pointOneTop, pointOneRight, pointOneBottom);
-        pointTwoRect = new RectF(pointTwoLeft, pointTwoTop, pointTwoRight, pointTwoBottom);
-        pointThreeRect = new RectF(pointThreeLeft, pointThreeTop, pointThreeRight, pointThreeBottom);
-        pointFourRect = new RectF(pointFourLeft, pointFourTop, pointFourRight, pointFourBottom);
-        pointFiveRect = new RectF(pointFiveLeft, pointFiveTop, pointFiveRight, pointFiveBottom);
     }
 
     public void clearDrawing() {
@@ -142,13 +103,6 @@ public class TraceView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawPath(path, paint);
-
-        //draw points!
-        canvas.drawRect(pointOneRect, paint);
-        canvas.drawRect(pointTwoRect, paint);
-        canvas.drawRect(pointThreeRect, paint);
-        canvas.drawRect(pointFourRect, paint);
-        canvas.drawRect(pointFiveRect, paint);
     }
 
     @Override
@@ -185,22 +139,14 @@ public class TraceView extends View {
                 path.lineTo(x, y);
                 particleSystem.updateEmitPoint((int) x, (int) y);
 
-                if (dirtyRect.intersect(pointOneRect)) {
-                    Log.e(TAG, "Point One Intersected");
-                    Toast.makeText(activity, "Point One Intersected", Toast.LENGTH_SHORT).show();
-                } else if (dirtyRect.intersect(pointTwoRect)) {
-                    Log.e(TAG, "Point Two Intersected");
-                    Toast.makeText(activity, "Point Two Intersected", Toast.LENGTH_SHORT).show();
-                } else if (dirtyRect.intersect(pointThreeRect)) {
-                    Log.e(TAG, "Point Three Intersected");
-                    Toast.makeText(activity, "Point Three Intersected", Toast.LENGTH_SHORT).show();
-                } else if (dirtyRect.intersect(pointFourRect)) {
-                    Log.e(TAG, "Point Four Intersected");
-                    Toast.makeText(activity, "Point Four Intersected", Toast.LENGTH_SHORT).show();
-                } else if (dirtyRect.intersect(pointFiveRect)) {
-                    Log.e(TAG, "Point Five Intersected");
-                    Toast.makeText(activity, "Point Five Intersected", Toast.LENGTH_SHORT).show();
-                }
+                float percentageX = x / windowSize.x;
+                float percentageY = y/ windowSize.y;
+                float targetX = backgroundBitmap.getWidth() * percentageX;
+                float targetY = backgroundBitmap.getHeight() * percentageY;
+                int color = backgroundBitmap.getPixel((int)targetX, (int)targetY);
+                Timber.d("#%06X  %d", (0xFFFFFF & color), color);
+
+                //TODO COUNT DEM POINTS
                 
                 break;
             default:
